07409711bf391853f5c5d4cde49896290254f5a6
Mathis et Yohan <mathisetyohan@macbookpro13.home>
2022-01-12 15:56:04 +0100

    Add a second buzzer turn mode

--- RLS_V1.0/R2Home_SOFTWARE_V1.01/R2Home_OBC_V1.01/R2Home_OBC_V1.01.ino
+++ RLS_V1.0/R2Home_SOFTWARE_V1.01/R2Home_OBC_V1.01/R2Home_OBC_V1.01.ino
@@ -12,7 +12,8 @@
 // ----------------------------------- SETUP PANEL ----------------------------------- // 
 
 #define i_want_to_fly   false // Simulated servo movement to test the servo movement :)) 
-#define buzzer_turn     false // Buzzer sounds as function of the turn command 
+#define buzzer_turn     true  // Buzzer sounds as function of the turn command 
+#define buzzer_sweep    true  // Buzzer turn on steroïds, should be easier to understand his tricky language ^^
 #define no_init         false // Skip init, for testing only purposes 
 #define rc_mode         1     // 0 = only roll, 1 = pitch and roll mixed, 2 = pitch and roll separated
 #define autopilot_mode  1     // Control linear or with "hands up"
@@ -111,6 +112,19 @@ int lastled = 0;
 
 // MISC // 
 int buzzer = 2;
+int mid_freq = 0; 
+float time_sweep = 0; 
+unsigned long sweep_start = 0; 
+unsigned long tbeep = 0; 
+bool new_sweep = false; 
+bool current_sweep = false;
+int current_freq = 0; 
+float sweep_step = 1;
+float time_step = 1; 
+
+int beep_start = 0; 
+int beep_stop = 0; 
+int direc = 0; 
 int cam = 23; 
 int sw = 22; 
 int crash_count = 0; 
@@ -1150,6 +1164,7 @@ void userinter() {
 
 // -------------------------- Buzzer -------------------------- //
 
+  sweep_beep_update();
   EasyBuzzer.update();
   
 
@@ -1157,14 +1172,31 @@ void userinter() {
 
     if (millis()-last_waypoint_time > 2000) {
     
-      int tone_turn = map(steer_auto, 1000, 2000, 1000, 3000); 
-      double t_down    = abs(map(steer_auto, 1000, 2000, -9, 9))+1; 
-      t_down = (1/t_down)*500;
-
-      if ((millis()-t_turn)>t_down) { 
-        t_turn = millis(); 
-        EasyBuzzer.singleBeep(tone_turn,10);
-      } 
+      int tone_turn = map(steer_auto, 1000, 2000, 1000, 2000); 
+     
+      if (buzzer_sweep == true) {
+        double t_down    = abs(map(steer_auto, 1000, 2000, -3, 3))+1; 
+        t_down = (1/t_down)*1500;
+        if ((millis()-t_turn)>t_down) { 
+          t_turn = millis(); 
+          int dir = 0; 
+          if (tone_turn > 1500) { dir = 0; }
+          else                  { dir = 1; }
+          float force = 0; 
+          if (tone_turn > 1500) { force = tone_turn - 1500; } 
+          else                  { force = 1500 - tone_turn; }
+          force = constrain(force, 10, 200); 
+          sweep_beep_set(tone_turn, force, dir); 
+       }
+     }
+     else {
+      double t_down    = abs(map(steer_auto, 1000, 2000, -5, 5))+1; 
+       t_down = (1/t_down)*500;
+       if ((millis()-t_turn)>(t_down)) {
+          t_turn = millis(); 
+          EasyBuzzer.singleBeep(tone_turn, 20); 
+       }
+     } 
    }
  }
   
@@ -1203,6 +1235,8 @@ void userinter() {
 
 void setled(int a, int b, int c, int d, int e) { 
   
+  
+  
   if (batt_critical == true) { a = 255; b = 0; c = 0; d = 25; e = 50; }
   if (batt_low == true) { a = 255; b = 0; c = 0; d = 25; e = 200; }
 
@@ -1212,6 +1246,49 @@ void setled(int a, int b, int c, int d, int e) {
     duration = d; 
     timeled = millis(); 
  }
+}
+
+void sweep_beep_update() {
+  if (direc == 0) {
+    if (new_sweep == true) { 
+      sweep_start = millis(); 
+      new_sweep = false; 
+      current_sweep = true; 
+      current_freq = mid_freq-((sweep_step*time_sweep)/(2*time_step)); 
+      EasyBuzzer.singleBeep((current_freq),time_step);
+    }
+  
+    if ((current_sweep == true) and ((millis()-sweep_start)>=(time_step)) and (current_freq<mid_freq+((sweep_step*time_sweep)/(2*time_step)))) {
+      sweep_start = millis(); 
+      current_freq = current_freq + sweep_step;  
+      EasyBuzzer.singleBeep((current_freq),time_step);
+      beep_stop = millis(); 
+    }
+  }
+  else {
+    if (new_sweep == true) { 
+      sweep_start = millis(); 
+      new_sweep = false; 
+      current_sweep = true; 
+      current_freq = mid_freq+((sweep_step*time_sweep)/(2*time_step)); 
+      EasyBuzzer.singleBeep((current_freq),time_step);
+    }
+  
+    if ((current_sweep == true) and ((millis()-sweep_start)>=(time_step)) and (current_freq>mid_freq-((sweep_step*time_sweep)/(2*time_step)))) {
+      sweep_start = millis(); 
+      current_freq = current_freq - sweep_step;  
+      EasyBuzzer.singleBeep((current_freq),time_step);
+      beep_stop = millis(); 
+    }
+  } 
+}
+
+void sweep_beep_set(int freq, int dur, int dir) {
+  mid_freq = freq; 
+  time_sweep = dur; 
+  new_sweep = true; 
+  direc = dir; 
+  beep_start = millis(); 
 }
 
 void updateled() { 
@@ -1365,7 +1442,7 @@ void newfile() {
 }
 
 void sim() { 
-  sim_cmd_time = (millis()*(PI/4000));
+  sim_cmd_time = (millis()*(PI/10000));
   sim_cmd = sin(sim_cmd_time); 
   sim_cmd = map(sim_cmd, -1, 1, -180, 180); 
 }
